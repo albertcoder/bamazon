fcae2e35f943e7fa82c0de3ac78351d3adff5fa8
Albert Coder <eralbert9191@gmail.com>
2018-11-13 01:40:36 -0800

    added manager folder

---
+++ manager/bamazonManager.js
@@ -0,0 +1,32 @@
+// npm packages used
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "bamazon"
+});
+
+// Creates the connection with the server and loads the manager menu upon a successful connection
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+  }
+  grabProducts();
+});
+
+
+
+
+
+
+
+
+  
+  
+  
+
